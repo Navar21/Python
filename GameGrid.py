193b9fabbacf5530a572d35e3481e527820bdb86
Navar21 <67078834+Navar21@users.noreply.github.com>
2021-07-19 19:08:06 -0700

    Add files via upload

---
+++ GameGrid.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 18:50:36 2021
+
+@author: mrbar
+"""
+import pygame
+
+pygame.init()
+
+width = 25
+height = 28
+margin = 1
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+FPS = 60
+
+clock = pygame.time.Clock()
+
+window = (495, 610)
+screen = pygame.display.set_mode(window)
+
+PacManLeft = pygame.transform.scale(pygame.image.load("PacManLeft.png"), (width - 2, height - 2))
+PacManLeftRect = PacManLeft.get_rect(topleft = ((window[0] // 2) - 13, (window[1] // 2) + 16))
+PacManRight = pygame.transform.scale(pygame.image.load("PacManRight.png"), (width - 2, height - 2))
+PacManRightRect = PacManLeft.get_rect(topleft = ((window[0] // 2) - 13, (window[1] // 2) + 16))
+PacManUp = pygame.transform.scale(pygame.image.load("PacManUp.png"), (width - 2, height - 2))
+PacManUpRect = PacManLeft.get_rect(topleft = ((window[0] // 2) - 13, (window[1] // 2) + 16))
+PacManDown = pygame.transform.scale(pygame.image.load("PacManDown.png"), (width - 2, height - 2))
+PacManDownRect = PacManLeft.get_rect(topleft = ((window[0] // 2) - 13, (window[1] // 2) + 16))
+background = pygame.transform.scale(pygame.image.load('Background.png'), window)
+
+current_image = PacManLeft
+current_rect_image = PacManLeftRect
+
+matrix = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+          [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+          [1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1],
+          [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1], 
+          [1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,1],
+          [1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
+          [1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1],
+          [1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1],
+          [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
+          [0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
+          [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
+          [1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1],
+          [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
+          [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+          [1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1],
+          [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+          [1,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1],
+          [1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
+          [1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1],
+          [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+          [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+
+walls_list = []
+for row in range(len(matrix)):
+    for column in range(len(matrix[0])):
+        if matrix[row][column] == 1:
+            color = WHITE
+        if matrix[row][column] == 0:
+            color = BLACK
+        if color == WHITE:
+            walls_list.append(pygame.draw.rect(screen, color, 
+                             [(margin + width) * column + margin, 
+                              (margin + height) * row + margin,
+                              width, height]))
+
+def WallDetection(x, y, current_rect_image):
+    current_rect_image.right += x
+    print(current_rect_image.right)
+    for wall in range(len(walls_list)):
+        if current_rect_image.colliderect(walls_list[wall]):
+            print('Boom')
+            if x < 0: 
+                print('left')
+                current_rect_image.left = walls_list[wall].right
+            elif x > 0:
+                print('right')
+                current_rect_image.right = walls_list[wall].left
+            break
+
+    current_rect_image.top += y
+    for wall in range(len(walls_list)):
+        if current_rect_image.colliderect(walls_list[wall]):
+            print('Boom')
+            if y < 0:
+                print('up')
+                current_rect_image.top = walls_list[wall].bottom
+            elif y > 0:
+                print('down')
+                current_rect_image.bottom = walls_list[wall].top
+            break
+                        
+def Teleport(current_image_rect):
+    if current_image_rect.right > window[0] + 20:
+        current_image_rect.right = 0
+    if current_image_rect.right < 0:
+        current_image_rect.right = window[0] + 20
+                
+run = True
+while True:
+    clock.tick(10000)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+        screen.blit(background, (0, 0))
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            current_image = PacManLeft
+            PacManLeftRect = current_rect_image
+            WallDetection(-5, 0, current_rect_image)
+            Teleport(PacManLeftRect)
+        if key[pygame.K_RIGHT]:
+            current_image = PacManRight
+            PacManRightRect = current_rect_image
+            WallDetection(5, 0, current_rect_image)
+            Teleport(PacManRightRect)  
+        if key[pygame.K_UP]:
+            current_image = PacManUp
+            PacManUpRect = current_rect_image
+            WallDetection(0, -5, current_rect_image)
+        if key[pygame.K_DOWN]:
+            current_image = PacManDown
+            PacManDownRect = current_rect_image
+            WallDetection(0, 5, current_rect_image)
+
+    screen.blit(current_image, current_rect_image)
+    pygame.display.update()
+pygame.quit()
